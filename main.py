93ca3b8ffec203328975adaf654cdc681c822f38
Bell_e601 <Bell_e601@mail.ru>
2023-03-09 10:46:32 +0600

    add try-except

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 
 def parse(let):
-    alphabet = [
+    ALPHABET = [
         'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л',
         'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц',
         'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я']
-    index = alphabet.index(let.upper())
-    url = 'https://www.omgtu.ru/university/about-the-university/persons/index.php?b=' + str(index) # передаем необходимы URL адрес
-    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
-    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
-
+    if let.upper() not in ALPHABET:
+        let = 'А'
+    index = ALPHABET.index(let.upper())
+    URL = 'https://www.omgtu.ru/university/about-the-university/persons/index.php?b=' + str(index)
+    
+    try:
+        page = requests.get(URL)
+    except Exception as _ex:
+        print(_ex)
+        sys.exit(0)
+        
+    soup = BeautifulSoup(page.text, "html.parser")
     items = soup.findAll('div', style="padding: 5px; font-size: 120%;")
     workers = []
     for data in items:
-        #if data.find('a'):
         workers.append(data.text.replace('\n ', ''))
 
     return workers
@@ -24,6 +31,10 @@ workers = parse(let)
 print ('Если бы начнёте работать в Политехе, то будете находится среди этих людей: ')
 for worker in workers:
     print(worker, end='')
-file = open('Workers.txt', 'w')
-file.writelines(workers)
-file.close()
+try:
+    file = open('Workers.txt', 'w')
+    file.writelines(workers)
+except Exception as _ex:
+    print(_ex)
+finally:
+    file.close()
